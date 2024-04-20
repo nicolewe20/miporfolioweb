2e7e9347a0b522bdbebe9e724996d72f7186a5fa
Nicol Rotelaewe20 <166536745+nicolewe20@users.noreply.github.com>
2024-04-20 00:18:06 -0400

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,24 @@
+
+
+let a = 100;
+let b = 2345;
+let c = 39;
+let efe
+
+function cualesmayor(a,b,c){
+//manera mas facil de hacer//
+if (a>=b && a>=c){
+    efe=a
+}
+if (b>=a && b>=c){
+    efe=b
+}
+if (c>=a && c>=b){
+    efe=c
+}
+return efe
+}
+
+console.log(cualesmayor(a,b,c)) 
+
+
