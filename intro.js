0853e2fc9564917ee7dc850e52792cf8b51ae66e
Nicol Rotelaewe20 <166536745+nicolewe20@users.noreply.github.com>
2024-04-19 23:38:11 -0400

    Add files via upload
    JS. TAREA FUNCIONES

---
+++ intro.js
@@ -0,0 +1,23 @@
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+let efe
+
+function cualesmayor(a,b,c){
+//manera mas facil de hacer//
+if (a>=b && a>=c){
+    efe=a
+}
+if (b>=a && b>=c){
+    efe=b
+}
+if (c>=a && c>=b){
+    efe=c
+}
+return efe
+}
+
+console.log(cualesmayor(a,b,c)) 
+
